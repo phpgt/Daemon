13c9808337e370e24c34d60be401bfb77fd331ef
Greg Bowler <greg.bowler@g105b.com>
2019-03-26 12:55:34 +0000

    Test Pool::readError

--- test/unit/PoolTest.php
+++ test/unit/PoolTest.php
@@ -94,4 +94,44 @@ class PoolTest extends TestCase {
 			$error
 		);
 	}
+
+	public function testReadError() {
+		/** @var MockObject|Process $proc1 */
+		$proc1 = self::createMock(Process::class);
+		$proc1->method("getOutput")
+		->will($this->returnCallback(function(int $pipe) {
+			if($pipe === Process::PIPE_OUT) {
+				return "Here is some output from proc1";
+			}
+			else {
+				return "Here is an error from proc1";
+			}
+		}));
+
+		/** @var MockObject|Process $proc2*/
+		$proc2= self::createMock(Process::class);
+		$proc2->method("getOutput")
+		->will($this->returnCallback(function(int $pipe) {
+			if($pipe === Process::PIPE_OUT) {
+				return "Here is some output from proc2";
+			}
+			else {
+				return "Here is an error from proc2";
+			}
+		}));
+
+		$sut = new Pool();
+		$sut->add("test1", $proc1);
+		$sut->add("test2", $proc2);
+
+		$error = $sut->readError();
+		self::assertStringContainsString(
+			"[test2 ERROR] Here is an error from proc2",
+			$error
+		);
+		self::assertStringContainsString(
+			"[test1 ERROR] Here is an error from proc1",
+			$error
+		);
+	}
 }
