473842819f9a9dc70174eca7220dc6c9214ebb44
Greg Bowler <greg.bowler@g105b.com>
2019-03-26 12:37:09 +0000

    Test adding and executing processes in Pool

--- test/unit/PoolTest.php
+++ test/unit/PoolTest.php
@@ -1,10 +1,26 @@
 <?php
 namespace Gt\Daemon\Test;
 
+use Gt\Daemon\Pool;
+use Gt\Daemon\Process;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class PoolTest extends TestCase {
-	public function testTest() {
-		self::assertTrue(true);
+	public function testAddExec() {
+		/** @var MockObject|Process $proc1 */
+		$proc1 = self::createMock(Process::class);
+		$proc1->expects($this->once())
+			->method("exec");
+
+		/** @var MockObject|Process $proc2 */
+		$proc2 = self::createMock(Process::class);
+		$proc2->expects($this->once())
+			->method("exec");
+
+		$sut = new Pool();
+		$sut->add("test1", $proc1);
+		$sut->add("test2", $proc2);
+		$sut->exec();
 	}
 }
